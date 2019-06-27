04704a9aa09c763f8da5c0a6089d2b5053e8fe1a
Eder Pires <ederpbj@gmail.com>
2019-06-27 17:07:24 -0300

    A04-Estrutura inicial e Preload (1.2)

---
+++ Modulo 16/A04-Estrutura inicial e Preload (1.2)/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+
+import Reducers from './src/Reducers';
+
+import Preload from './src/Preload';
+import Home from './src/Home';
+
+let store = createStore(Reducers, applyMiddleware(ReduxThunk));
+
+const Navegador = createStackNavigator({
+  Preload:Preload,
+  Home:Home
+	
+});
+
+export default class App extends Component {
+	render(){
+		return(
+			<Provider store={store}>
+				<Navegador />
+			</Provider>
+		)
+	}
+}
---
+++ Modulo 16/A04-Estrutura inicial e Preload (1.2)/src/reducers/AuthReducer.js
@@ -0,0 +1,13 @@
+const initialState ={
+    email:'',
+    password:''
+};
+
+const AuthReducer = (state = initialState, action) => {
+
+
+    return state;
+    
+};
+
+export default AuthReducer;
