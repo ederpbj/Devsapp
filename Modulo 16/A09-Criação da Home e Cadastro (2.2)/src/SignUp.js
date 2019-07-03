24fa91923f7087354db0af1c1d8a19f42d534b57
Eder Pires <ederpbj@gmail.com>
2019-07-02 22:36:49 -0300

    A09-Criação da Home e Cadastro (2.2)

--- Modulo 16/A09-Criação da Home e Cadastro (2.2)/src/SignUp.js
+++ Modulo 16/A09-Criação da Home e Cadastro (2.2)/src/SignUp.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from './node_modules/react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
-import { connect } from 'react-redux';
+import { connect } from './node_modules/react-redux';
 import { checkLogin, changeEmail, changePassword, changeName, signUpAction } from './actions/AuthActions';
 
 export class SignUp extends Component {
