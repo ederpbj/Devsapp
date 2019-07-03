5c023f148d069dc8e5ea92b8e58f0c71b620e881
Eder Pires <ederpbj@gmail.com>
2019-07-02 21:53:34 -0300

    A08-Criação da Home e Cadastro (1.2)

---
+++ Modulo 16/A08-Criação da Home e Cadastro (1.2)/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { createStackNavigator, createAppContainer  } from 'react-navigation';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+
+import Reducers from './src/Reducers';
+
+import Preload from './src/Preload';
+import Home from './src/Home';
+import Conversas from './src/Conversas';
+import SignUp from './src/SignUp';
+
+let store = createStore(Reducers, applyMiddleware(ReduxThunk));
+
+const Navegador = createStackNavigator({
+  Preload:Preload,
+  Home:Home,
+  Conversas:Conversas,
+  SignUp:SignUp
+});
+
+
+export default class App extends Component {
+	render(){
+		return(
+			<Provider store={store}>
+				<Container />
+			</Provider>
+		)
+	}
+}
+
+const Container = createAppContainer(Navegador);
+//export default container; 
---
+++ Modulo 16/A08-Criação da Home e Cadastro (1.2)/src/Preload.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { StackActions, NavigationActions } from 'react-navigation';
+import { connect } from 'react-redux';
+import { checkLogin } from './actions/AuthActions';
+
+export class Preload extends Component {
+
+    static navigationOptions = {
+        title:'',
+        header:null
+    }
+
+    constructor(props){
+        super(props);
+        this.state = {}
+
+        this.props.checkLogin();
+
+        this.directPages = this.directPages.bind(this);
+    }
+
+    
+
+    directPages(){
+        //Navegação comum
+        //this.props.navigation.navigate('Home');
+
+        switch (this.props.status) {
+            case 1:
+
+                //Zera a posição vai para Conversas
+                this.props.navigation.dispatch(StackActions.reset({
+                    index: 0,
+                    actions: [
+                        NavigationActions.navigate({routeName: "Conversas"})
+                    ]
+                })
+
+            );
+                break;
+           
+            case 2:
+
+                //Zera a posição vai para Home
+                this.props.navigation.dispatch(StackActions.reset({
+                    index: 0,
+                    actions: [NavigationActions.navigate({
+                        routeName: "Home"
+                    })]
+                })
+
+            );
+
+                break;
+        
+            default:
+                break;
+        }
+
+    }
+
+    //Chama component depois que a tela atualiza
+    //componentWillUpdate
+
+    //Quando a app é re-gerado, abrir novamente app
+    componentDidMount(){
+        this.directPages();
+    }
+
+    //Chama component gera novamente o render, atualizar página
+    componentDidUpdate(){
+        this.directPages();
+    }
+
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text>Carregando ... {this.props.status}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        margin:10
+    }
+});
+
+const mapStateToProps = (state) => {
+    return {
+        status:state.auth.status
+    };
+};
+
+//Constante de conexão (Preload)->Tela que vai abrir
+const PreloadConnect = connect(mapStateToProps, { checkLogin })(Preload);
+export default PreloadConnect;
